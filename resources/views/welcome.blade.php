66f337074b486c075684d615534eacd8439e98b1
Daniel Fan <ifanqing@gmail.com>
2018-09-28 17:57:03 +0000

    update for Daniel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Qing</div>
+                <div class="title">Hello Laravel! - by Daniel</div>
             </div>
         </div>
     </body>
